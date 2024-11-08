3db30a7ea40dcace8b694e8e3b96ccebbe8115b1
Carlos Pereira <dawcarlospereira@gmail.com>
2024-11-08 18:32:01 +0100

    Primer commit

---
+++ src/main/java/iesjuanbosco/compraventawallapop/CompraVentaWallapopApplication.java
@@ -0,0 +1,13 @@
+package iesjuanbosco.compraventawallapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraVentaWallapopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompraVentaWallapopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iesjuanbosco/compraventawallapop/CompraVentaWallapopApplicationTests.java
@@ -0,0 +1,13 @@
+package iesjuanbosco.compraventawallapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraVentaWallapopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
